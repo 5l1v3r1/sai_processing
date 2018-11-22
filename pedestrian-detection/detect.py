9d6b4dd5e891bb39bc4da318c5c187f3ae8bf15d
Kolsha <ovchinnikovnikola@mail.ru>
2018-11-22 12:07:55 +0700

    19-21 passed

--- pedestrian-detection/detect.py
+++ pedestrian-detection/detect.py
@@ -10,49 +10,45 @@ import argparse
 import imutils
 import cv2
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--images", required=True, help="path to images directory")
-args = vars(ap.parse_args())
 
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 # loop over the image paths
-imagePaths = list(paths.list_images(args["images"]))
+cap = cv2.VideoCapture('/Users/kolsha/Documents/Projects/Python/sai_processing/videos/vtest.avi')
+while(1):
+        ret, frame = cap.read()
+        if frame is None:
+            break
+        image = imutils.resize(frame, width=min(frame.shape[0], frame.shape[1]))
+        orig = image.copy()
 
-for imagePath in imagePaths:
-	# load the image and resize it to (1) reduce detection time
-	# and (2) improve detection accuracy
-	image = cv2.imread(imagePath)
-	image = imutils.resize(image, width=min(400, image.shape[1]))
-	orig = image.copy()
+        # detect people in the image
+        (rects, weights) = hog.detectMultiScale(image, winStride=(8,8), padding=(32,32), scale=1.005)
 
-	# detect people in the image
-	(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
-		padding=(8, 8), scale=1.05)
+        # draw the original bounding boxes
+        for (x, y, w, h) in rects:
+            cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-	# draw the original bounding boxes
-	for (x, y, w, h) in rects:
-		cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # apply non-maxima suppression to the bounding boxes using a
+        # fairly large overlap threshold to try to maintain overlapping
+        # boxes that are still people
+        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.165)
 
-	# apply non-maxima suppression to the bounding boxes using a
-	# fairly large overlap threshold to try to maintain overlapping
-	# boxes that are still people
-	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+        # draw the final bounding boxes
+        for (xA, yA, xB, yB) in pick:
+            cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
-	# draw the final bounding boxes
-	for (xA, yA, xB, yB) in pick:
-		cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+        # show some information on the number of bounding boxes
+        # filename = imagePath[imagePath.rfind("/") + 1:]
+        # print("[INFO] {}: {} original boxes, {} after suppression".format(
+        #     filename, len(rects), len(pick)))
 
-	# show some information on the number of bounding boxes
-	filename = imagePath[imagePath.rfind("/") + 1:]
-	print("[INFO] {}: {} original boxes, {} after suppression".format(
-		filename, len(rects), len(pick)))
-
-	# show the output images
-	cv2.imshow("Before NMS", orig)
-	cv2.imshow("After NMS", image)
-	cv2.waitKey(0)
+        img = np.concatenate((orig, image), axis=1)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        cv2.imshow("Ped", img)
+        k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+        if k == 27:
+        	break
